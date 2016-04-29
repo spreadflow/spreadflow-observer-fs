4c6fcb10deb2930f53cc628f113ad59e762b56f1
Lorenz Schori <lo@znerol.ch>
2016-04-29 21:54:52 +0200

    Fix automatic detection of observer command, raise an exception if not found

--- twisted/plugins/spreadflow_observer_fs_endpoint.py
+++ twisted/plugins/spreadflow_observer_fs_endpoint.py
@@ -23,30 +23,34 @@ class SpreadflowObserverFSProcessEndpoint(object):
         return "spreadflow-observer-fs-%s" % type
 
 
-    def _find_executable(self, type):
-        binary_name = self._binary_name(type)
-        executable = which(binary_name)
+    def _find_executable(self, binary_name):
+        executable = None
+        candidates = which(binary_name)
 
-        if not executable:
-            # walk up the directory hierarchy and try to find the path to a
-            # helper tool built with buildout.
-            executable = os.path.abspath(__file__)
-            while os.path.dirname(executable) != executable:
-                candidate = os.path.join(executable, 'bin', binary_name)
+        if len(candidates) > 0:
+            executable = candidates[0]
+        else:
+            # Walk up the directory hierarchy and try to find the path to a
+            # helper tool built with buildout or installed into a virtualenv.
+            path = os.path.abspath(__file__)
+            while os.path.dirname(path) != path:
+                candidate = os.path.join(path, 'bin', binary_name)
                 if os.path.exists(candidate):
                     executable = candidate
                     break
-                executable = os.path.dirname(executable)
-        else:
-            executable = None
+                path = os.path.dirname(path)
 
         return executable
 
 
     def _parse(self, reactor, directory, query, native_query=True, type=None, executable=None):
+        binary_name = self._binary_name(type)
 
         if not executable:
-            executable = self._find_executable(type)
+            executable = self._find_executable(binary_name)
+
+        if not executable:
+            raise ValueError("Failed to find command {:s}".format(binary_name))
 
         args = (executable,)
         if ast.literal_eval(str(native_query)):
