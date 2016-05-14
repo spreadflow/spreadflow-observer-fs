c40ca740aa22c5a848f1198595c6d83cf4f62094
Lorenz Schori <lo@znerol.ch>
2016-05-14 21:40:18 +0200

    Explicitly specify parser factory

--- spreadflow_observer_fs/source.py
+++ spreadflow_observer_fs/source.py
@@ -2,9 +2,11 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import unicode_literals
 
-from spreadflow_core.client import SchedulerClient
+from spreadflow_core.client import SchedulerClient, MessageParser
 
 class FilesystemObserverSource(SchedulerClient):
+
+    parser_factory = MessageParser
 
     def __init__(self, query, directory):
         self.endpoint = 'spreadflow-observer-fs:' + ':'.join([directory, query])
