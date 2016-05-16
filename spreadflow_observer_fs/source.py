06880995f61bb8eecc23fd6b6eeb013c5ad86ac2
Lorenz Schori <lo@znerol.ch>
2016-05-16 18:30:37 +0200

    Update for changed core-api

--- spreadflow_observer_fs/source.py
+++ spreadflow_observer_fs/source.py
@@ -2,16 +2,19 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import unicode_literals
 
-from spreadflow_core.client import SchedulerClient
+from spreadflow_core.remote import MessageHandler, SchedulerClientFactory, \
+    SchedulerProtocol, ClientEndpointMixin
 from spreadflow_format_bson import MessageParser
 
-class FilesystemObserverSource(SchedulerClient):
-
-    parser_factory = MessageParser
+class FilesystemObserverSource(ClientEndpointMixin):
 
     def __init__(self, query, directory):
-        self.endpoint = 'spreadflow-observer-fs:' + ':'.join([directory, query])
-        self.portmap = {'default': self}
+        self.strport = 'spreadflow-observer-fs:' + ':'.join([directory, query])
+
+    def get_client_protocol_factory(self, scheduler, reactor):
+        handler = MessageHandler(scheduler, {'default': self})
+        return SchedulerClientFactory.forProtocol(
+            SchedulerProtocol, handler=handler, parser_factory=MessageParser)
 
     def __call__(self, item, send):
         send(item, self)
