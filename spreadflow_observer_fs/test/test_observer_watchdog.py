aa6262e0c91e5fa8c09b579b27e54702f9bff81f
Lorenz Schori <lo@znerol.ch>
2016-10-05 10:22:44 +0200

    Use StreamsReader helper from core

--- spreadflow_observer_fs/test/test_observer_watchdog.py
+++ spreadflow_observer_fs/test/test_observer_watchdog.py
@@ -20,71 +20,8 @@ import time
 import unittest
 
 from bson import BSON
+from spreadflow_core.test.util import StreamsReader
 
-try:
-    import queue
-except ImportError:
-    import Queue as queue
-
-MAXWAIT = 5.0
-
-class _TimeoutError(Exception):
-    pass
-
-class _StreamReader(object):
-    """
-    A non-blocking reader for multiple input streams.
-    """
-    def __init__(self, streams):
-        self._queue = queue.Queue()
-        self._streams = streams
-        self._threads = None
-
-    def drain(self, timeout=MAXWAIT):
-        if timeout > 0:
-            deadline = time.time() + timeout
-
-        while True:
-            if timeout > 0 and time.time() > deadline:
-                raise _TimeoutError()
-
-            try:
-                stream, data = self._queue.get(timeout=timeout)
-            except queue.Empty:
-                break
-            else:
-                yield stream, data
-
-    def start(self):
-        for stream in self._streams:
-            thread = threading.Thread(target=self._reader, args=[stream])
-            thread.daemon = True
-            thread.start()
-
-    def join(self):
-        if self._threads is not None:
-            for thread in self._threads:
-                thread.join()
-
-    def _reader(self, stream):
-        """
-        Performs non-blocking read from a stream. Use it in a thread.
-        """
-        flags = fcntl.fcntl(stream, fcntl.F_GETFL)
-        fcntl.fcntl(stream, fcntl.F_SETFL, flags | os.O_NONBLOCK)
-
-        while True:
-            try:
-                data = stream.read()
-            except (OSError, IOError) as e:
-                if e.errno == errno.EINTR or e.errno == errno.EAGAIN:
-                    continue
-                raise
-            else:
-                if data is not None:
-                    self._queue.put((stream, data))
-                    if data == b'':
-                        break
 
 class SpreadflowObserverIntegrationTestCase(unittest.TestCase):
     """
@@ -114,7 +51,7 @@ class SpreadflowObserverIntegrationTestCase(unittest.TestCase):
 
             stream_data = {proc.stdout: b'', proc.stderr: b''}
 
-            reader = _StreamReader([proc.stdout, proc.stderr])
+            reader = StreamsReader([proc.stdout, proc.stderr])
             reader.start()
 
             # Write a file.
