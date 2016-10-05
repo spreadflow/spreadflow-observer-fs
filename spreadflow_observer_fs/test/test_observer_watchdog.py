1ff7c9b19f476ea6e7ac9ef645ccfc5776862f2f
Lorenz Schori <lo@znerol.ch>
2016-10-05 09:43:32 +0200

    Add move operation to integration test

--- spreadflow_observer_fs/test/test_observer_watchdog.py
+++ spreadflow_observer_fs/test/test_observer_watchdog.py
@@ -133,18 +133,7 @@ class SpreadflowObserverIntegrationTestCase(unittest.TestCase):
             else:
                 self.fail('Observer process is expected to emit a message to stdout{0}'.format(self._format_stream('*** BINARY ***', stream_data[proc.stderr])))
 
-            # Close stdin, this signals the observer process to terminate.
-            proc.stdin.close()
-
-            proc.wait()
-            self.assertEqual(proc.returncode, 0, self._format_stream('*** BINARY ***', stream_data[proc.stderr]))
-
-            for stream, data in reader.drain(0):
-                stream_data[stream] += data
-
-            reader.join()
-
-            # Verify output.
+            # Verify output (create operation).
             msg = BSON(stream_data[proc.stdout]).decode()
             self.assertIn('item', msg)
             self.assertIn('port', msg)
@@ -164,7 +153,67 @@ class SpreadflowObserverIntegrationTestCase(unittest.TestCase):
             self.assertEqual(len(item['inserts']), 1)
             self.assertEqual(len(item['deletes']), 0)
 
-            key = item['inserts'][0]
-            self.assertIn(key, item['data'])
+            origkey = item['inserts'][0]
+            self.assertIn(origkey, item['data'])
 
-            self.assertEqual(item['data'][key]['path'], txtpath)
+            self.assertEqual(item['data'][origkey]['path'], txtpath)
+
+            # Reset stdout buffer.
+            stream_data[proc.stdout] = b''
+
+            # Move a file.
+            newpath = os.path.join(rundir, 'test-2.txt')
+            os.rename(txtpath, newpath)
+
+            for stream, data in reader.drain():
+                stream_data[stream] += data
+                if stream_data[proc.stdout]:
+                    break
+            else:
+                self.fail('Observer process is expected to emit a message to stdout{0}'.format(self._format_stream('*** BINARY ***', stream_data[proc.stderr])))
+
+            # Verify output (mv operation).
+            msg = BSON(stream_data[proc.stdout]).decode()
+            self.assertIn('item', msg)
+            self.assertIn('port', msg)
+
+            item = msg['item']
+            self.assertEqual(msg['port'], 'default')
+
+            self.assertIn('data', item)
+            self.assertIn('inserts', item)
+            self.assertIn('deletes', item)
+
+            self.assertIsInstance(item['data'], collections.Mapping)
+            self.assertIsInstance(item['inserts'], collections.Sequence)
+            self.assertIsInstance(item['deletes'], collections.Sequence)
+
+            # FIXME: Apparently there is a data-entry also for deletes. Figure
+            # out whether this is really expected/necessary. Also compare to
+            # the behavior of spreadflow-observer-fs-spotlight.
+            self.assertEqual(len(item['data']), 2)
+            self.assertEqual(len(item['inserts']), 1)
+            self.assertEqual(len(item['deletes']), 1)
+
+            oldkey = item['deletes'][0]
+            newkey = item['inserts'][0]
+            self.assertIn(newkey, item['data'])
+
+            self.assertEqual(item['data'][newkey]['path'], newpath)
+            self.assertEqual(origkey, oldkey)
+            self.assertNotEqual(origkey, newkey)
+
+            # Reset stdout buffer.
+            stream_data[proc.stdout] = b''
+
+            # Close stdin, this signals the observer process to terminate.
+            proc.stdin.close()
+
+            proc.wait()
+            self.assertEqual(proc.returncode, 0, self._format_stream('*** BINARY ***', stream_data[proc.stderr]))
+
+            for stream, data in reader.drain(0):
+                stream_data[stream] += data
+
+            reader.join()
+
